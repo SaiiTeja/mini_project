2f2637ebc63f445e9116c0242dd0f175b688ad52
SaiiTeja <nallapanenisaiteja@gmail.com>
2025-03-02 11:45:59 +0530

    javascript updates

--- project.js
+++ project.js
@@ -32,6 +32,61 @@ function hideText() {
     }
 }
 function iris(){
-    let iri=document.getElementById("iriss");
-    iri.innerHTML=`<a href="1.csv" download>Download Now</a>`;
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h2><center>iris dataset</h2><br> <h3>1.file 1 <a href="datasets/1.csv" download class="fa fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/1.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function digits(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h2><center>written digits dataset</h2><br> <h3>1.file 1 <a href="datasets/2.csv" download class="fa fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/2.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function wine(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h2><center>wine datasets</h2><br> <h3>1.file 1 <a href="datasets/3.csv" download class="fa fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/3.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function news(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h2><center>20 types of new dataset</h2><br> <h3>1.file 1 <a href="datasets/4.csv" download class="fa fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/4.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function diabates(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>diabates dataset for regression</h3><br> <h3>1.file 1 <a href="datasets/5.csv" download class="fa fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/5.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}function house(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>house price prediction dataset</h3><br> <h3>1.file 1 <a href="datasets/6.csv" download class="fa-solid fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/6.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function face(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>facial recognization dataset</h3><br> <h3>1.file 1 <a href="datasets/7.csv" download class="fa-solid fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/7.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function forest(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>forest cover dataset for classification</h3><br> <h3>1.file 1 <a href="datasets/8.csv" download class="fa-solid fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/8.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function network(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>network intrusion dataset</h3><br> <h3>1.file 1 <a href="datasets/9.csv" download class="fa-solid fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/9.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
+}
+function circle(){
+    let datasets=document.getElementById("datasets");
+    datasets.innerHTML=`<h3><center>circle for non-linear classification</h3><br> <h3>1.file 1 <a href="datasets/10.csv" download class="fa-solid fa-download text-dark"></a></h3>
+        <h3>2.file 2 <a href="datasets/10.5.csv" download class="fa fa-download text-dark"></a></h3></center><p>Note:-
+        The file 1 contains the 10,000 rows of the data where the file 2 contains 1,000 rows of the data with 8 entities in each</p>`;
 }
